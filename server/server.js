57a347a6cc305c7b135a60ebc1b4e64449c3f7fd
antonio madureira <antonio.madureira2222@gmail.com>
2022-07-13 00:59:55 -0400

    start of the server file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("Tony");
+});
+
+app.listen(3001, () => {
+    console.log('running on port 3001');
+});
